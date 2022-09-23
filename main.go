beda307b79f043f5b5c2fa8c8f2905f018716339
takonomura <takonomura@users.noreply.github.com>
2022-09-23 11:43:30 +0900

    Use go embed for template

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"html/template"
 	"log"
 	"net/http"
@@ -10,45 +11,12 @@ import (
 	"robpike.io/nihongo"
 )
 
-var indexTemplate = template.Must(template.New("index").Parse(`<!DOCTYPE html>
-<html lang="ja">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>{{ .Title }}</title>
+var (
+	//go:embed index.html.tmpl
+	html string
+)
 
-	<link rel="icon" sizes="128x128" href="icon-128.png">
-
-	<meta property="og:title" content="{{ .Title }}">
-	<meta property="og:type" content="website">
-	<meta property="og:url" content="https://{{ .Domain }}/">
-	<meta property="og:image" content="https://{{ .Domain }}/icon-128.png">
-
-	<style>
-		html, body {
-			width: 100%;
-			height: 100%;
-		}
-
-		.centered {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			height: 100%;
-		}
-
-		h1 {
-			font-size: 6rem;
-		}
-	</style>
-</head>
-<body>
-	<div class="centered">
-		<h1>{{ .Title }}</h1>
-	</div>
-</body>
-</html>
-`))
+var htmlTemplate = template.Must(template.New("index").Parse(html))
 
 func main() {
 	log.Fatal(http.ListenAndServe("127.0.0.1:5000", http.HandlerFunc(handleHTTP)))
@@ -58,7 +26,7 @@ func handleHTTP(w http.ResponseWriter, r *http.Request) {
 	sub, domain := parseHost(r.Host)
 	title := convertSubdomain(sub) + "なさそう"
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	indexTemplate.Execute(w, struct {
+	htmlTemplate.Execute(w, struct {
 		Title  string
 		Domain string
 	}{
